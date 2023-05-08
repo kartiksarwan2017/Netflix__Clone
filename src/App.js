e6302faa0204acb880e841acbfb10022d89b520b
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-05-08 20:16:04 +0530

    Setting Up the Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.scss';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
